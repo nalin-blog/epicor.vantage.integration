5f7a734b9f48625062f048b30bc3372da5245933
Nalin Wickramasinghe <nalin.wickramsinghe@gmail.com>
2015-04-30 12:56:56 -0400

    initial commit
    Simple console application showing how to connect with epicor vista

---
+++ epicor.vantage.integration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace epicor.vantage.integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var epicorSession = new Epicor.Mfg.Core.Session("manager", "manager", "AppServerDC://EPICORVA803400:8311");
+                var epicorPartClass = new Epicor.Mfg.BO.PartClass(epicorSession.ConnectionPool);
+
+                bool morePages = false;
+                var datapartClasses = epicorPartClass.GetRows("", "", 1000, 0, out morePages);
+
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
